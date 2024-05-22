48babe4e0e8901ae8a45236b294c10cb473f2066
OSpoon <1825203636@qq.com>
2024-05-22 15:13:56 +0800

    feat: 添加 git-user 命令

--- src/constants.ts
+++ src/constants.ts
@@ -23,6 +23,9 @@ export const usage = [
   `${pc.cyan('clt')} ${pc.yellow('mkcert <domain> [<domain> ...]')}     ${pc.bold('generate a local trusted development certificate')}`,
   `${pc.cyan('clt')} ${pc.yellow('mkcert --uninstall')}                 ${pc.bold('uninstall the local CA (but do not delete it)')}`,
   `${pc.cyan('clt')} ${pc.yellow('kill <port>')}                        ${pc.bold('releases the process occupied by the port')}`,
+  `${pc.cyan('clt')} ${pc.yellow('git-user')}                           ${pc.bold('toggle git user information')}`,
+  `${pc.cyan('clt')} ${pc.yellow('git-user add')}                       ${pc.bold('add git user information')}`,
+  `${pc.cyan('clt')} ${pc.yellow('git-user remove')}                    ${pc.bold('delete git user information')}`,
 ].join('\n')
 
 export const CREATE_QUESTIONS: Array<QuestionCollection> = [
@@ -87,6 +90,83 @@ export function IPDLIST_QUESTIONS(processes: { protocol: string, local: string, 
         return {
           name: `${process.protocol} ${process.local}`,
           value: process.pid,
+        }
+      }),
+    },
+  ]
+}
+
+export const ADD_GIT_USER_QUESTIONS: Array<QuestionCollection> = [
+  {
+    type: 'input',
+    name: 'tag',
+    message: 'git user tag:',
+    validate(input: string): string | boolean {
+      if (!input)
+        return 'please enter the git user tag'
+      return true
+    },
+  },
+  {
+    type: 'input',
+    name: 'user',
+    message: 'git user:',
+    validate(input: string): string | boolean {
+      if (!input)
+        return 'please enter the git user'
+      return true
+    },
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'git email:',
+    validate(input: string): string | boolean {
+      if (!input)
+        return 'please enter the git email'
+      return true
+    },
+  },
+]
+
+export function USE_GIT_USER_QUESTIONS(users: { tag: string, user: string, email: string }[]): Array<QuestionCollection> {
+  return [
+    {
+      type: 'list',
+      name: 'info',
+      message: 'selected git user:',
+      choices: users.map((user) => {
+        const tagify = `[${user.tag}]${''.padEnd(10 - user.tag.length, ' ')}`
+        return {
+          name: `${tagify} ${user.user} ${user.email}`,
+          value: `${user.user} ${user.email}`,
+        }
+      }),
+    },
+    {
+      type: 'list',
+      name: 'scope',
+      message: 'selected git scope:',
+      choices: [
+        { name: 'project', value: '' },
+        // 结尾空格请勿删除
+        { name: 'global', value: '--global ' },
+      ],
+    },
+  ]
+}
+
+export function REMOVE_GIT_USER_QUESTIONS(users: { tag: string, user: string, email: string }[]): Array<QuestionCollection> {
+  return [
+    {
+      type: 'list',
+      name: 'info',
+      message: 'selected git user:',
+      choices: users.map((user) => {
+        const tagify = `[${user.tag}]${''.padEnd(10 - user.tag.length, ' ')}`
+        return {
+          name: `${tagify} ${user.user} ${user.email}`,
+          value: `${user.tag} ${user.user} ${user.email}`,
         }
       }),
     },
