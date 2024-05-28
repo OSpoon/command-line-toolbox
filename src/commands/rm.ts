0807f3edb02a369b533d7cf0464b6ea3d06f5a96
OSpoon <zxin088@gmail.com>
2024-05-28 16:42:40 +0800

    fix: 调整 rm 命令描述

--- src/commands/rm.ts
+++ src/commands/rm.ts
@@ -3,14 +3,14 @@ import { spinner } from '../constants'
 
 export default (path: string[]) => {
   const command = `npx rimraf ${path.join(' ')}`
-  spinner.start(`deleting ${path.join(' ')} ...`)
+  spinner.start(`rm -rf ${path.join(' ')} ...`)
   shell.exec(command, {
     fatal: true,
     silent: true,
   }, (code: number, _stdout: string, stderr: string) => {
     if (code === 0) {
       // Complete processing
-      spinner.succeed('deleted successful')
+      spinner.succeed('remove successful')
     }
     else {
       // Abnormal interrupt
